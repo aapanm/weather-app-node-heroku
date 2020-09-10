0245f38435becd38288edffd33455b8a8b3c1dc8
Apon <aapa.cuet.cse@gmail.com>
2020-09-10 15:11:46 +0600

    init commit

---
+++ weather-app/public/js/app.js
@@ -0,0 +1,33 @@
+// const baseUrl = new URL('/weather?address=')
+const baseUrl = '/weather?address=';
+
+
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-one');
+const messagetwo = document.querySelector('#message-two');
+
+weatherForm.addEventListener('submit', ()=>{
+    event.preventDefault();
+
+    const location = search.value;
+
+    // baseUrl.search = "";
+    // const params = {address:location}
+    // Object.keys(params).forEach(key => baseUrl.searchParams.append(key, params[key]))
+    
+    fetch(baseUrl+location).then((response)=>{
+    response.json().then((data)=>{
+       if(data.Error){
+           messageOne.textContent = data.Error;
+       }else{
+        messageOne.textContent = data.location;
+        messagetwo.textContent = data.forecast;
+       }
+    })
+})
+
+    console.log(location);
+})
